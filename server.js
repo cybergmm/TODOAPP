6aab18c7633bada79d3d3de045d9faa5f0f5cd49
Sumin Yu <spc4yoo@gmail.com>
2022-12-26 18:06:41 +0900

    추가 : DB와 데이터 연결 - mongoDB 연결 - ejs 라이브러리

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }))
+var db;
+const MongoClient = require('mongodb').MongoClient;
+app.set('view engine', 'ejs');
 
-app.listen(8080, function() {
+MongoClient.connect('mongodb+srv://admin:qC7Mnf6qNA6quWlj@cluster0.2afz5ax.mongodb.net/?retryWrites=true&w=majority', (error, client) => {
+// 연결되면 할일
+    if (error) {
+        return console.log(error);
+    }
+
+    db = client.db('todoapp');
+
+    // db.collection('post').insertOne({name: 'sumin', age:29}, (error, result) => {
+    //     console.log('저장완료');
+    // });
+
+    app.listen(8080, ()=> {
     console.log('listening on 8080');
-});
+    });
+
+})
+
+
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname+'/index.html');
@@ -23,5 +44,23 @@ app.get('/beauty', function (req, res) {
 });
 
 app.post('/add', (req, res) => {
-    res.send('전송완료')
+    res.send('전송완료');
+    console.log(req.body.title);
+    console.log(req.body.date);
+    db.collection('post').insertOne({ title: req.body.title, date:req.body.date}, (error, result) => {
+        console.log('저장완료');
+    });
+    // DB에 저장 
 })
+
+app.get('/list', (req, res) => {
+    
+    db.collection('post').find().toArray((err, result) => {
+        console.log(result);
+        res.render('list.ejs', { posts: result });
+    });
+    
+
+    // db에 저장된 post라는 collection안의 데이터를 꺼내자
+    
+});
